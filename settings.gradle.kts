7a97e7b089ad8a7f302206f9e46f7e928636da4b
Syamil <1201100421@student.mmu.edu.my>
2024-06-04 10:52:42 +0800

    Initial Commit plus UI Layout

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "muhd-syamil-quiz2"
+include(":app")
